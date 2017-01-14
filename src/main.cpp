735fa0ca4df30d11d9fa80040578b998f20e7d0d
Jay Wiggins <jay@irk.com>
2017-01-14 11:18:29 -0700

    final?

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
-#include <Adafruit_NeoPixel.h>
+// fuse settings...
+// avrdude -c usbtiny -p attiny85 -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
+
+#include <FastLED.h>
+#define NUM_LEDS 5
+#define DATA_PIN 0
+
 #include <avr/power.h>
 #include <avr/sleep.h>
-
- 
-#define PIN      0
-#define N_LEDS 5
-
 
 #define set(x) |= (1<<x)
 #define clr(x) &=~(1<<x) 
 #define inv(x) ^=(1<<x)
 
 void nightyNight();
-uint32_t Wheel(byte);
-uint8_t awakeCounter = 0;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+uint8_t awakeCounter = 110;
 
+// CRGB leds[NUM_LEDS];
+CRGBArray<NUM_LEDS> leds;
  
 void setup() {
   // power control pin...
@@ -34,8 +35,8 @@ void setup() {
   //   // set interrupt time to about 1 second and enable...
   // WDTCR = (1<<WDIE) | (1<<WDP2) | (1<<WDP1);
 
-  // set interrupt time to about 64ms and enable...
-	WDTCR = (1<<WDIE) | (1<<WDP0);
+  // set interrupt time to about 32ms and enable...
+  WDTCR = (1<<WDIE) | (1<<WDP0);
   
   // enable sleep mode for power down...
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
@@ -43,8 +44,8 @@ void setup() {
   // enable interrupts...
   sei();
   
-  strip.begin();
-  strip.setBrightness(40);
+  // FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  LEDS.addLeds<WS2812B,DATA_PIN, RGB>(leds, NUM_LEDS);
 }
 
 uint8_t ledsOn = 0;
@@ -53,32 +54,60 @@ void loop() {
   uint8_t currentPix = 0;
   uint8_t onTime = 0;
   
+  uint8_t hue = 0;
+  uint8_t hueDelta = 3;
+  
   for (;;) {
     nightyNight();
+    // digitalWrite(PORTB1, HIGH);
+    // // Turn the first led red for 1 second
+    // for(int dot = 0; dot < NUM_LEDS; dot++) {
+    //    leds[dot] = CRGB::Red;
+    //    FastLED.show();
+    //    // clear this led for the next time around the loop
+    //    leds[dot] = CRGB::Black;
+    //    delay(30);
+    // }
+
     // turn led pixels power on...
-               
-    if(awakeCounter > 120) {
+
+    if(awakeCounter == 0) {
+      // turn off
+      leds[0] = CRGB::Black;
+      leds[1] = CRGB::Black;
+      leds[2] = CRGB::Black;
+      leds[3] = CRGB::Black;
+      leds[4] = CRGB::Black;
+      // WDTCR = (1<<WDIE) | (0<<WDP0);
+      WDTCR = (1<<WDIE) | (1<<WDP0);
+      FastLED.show();
+      digitalWrite(PORTB1, LOW);      
+    }
+
+    if(awakeCounter > 200) {
       digitalWrite(PORTB1, HIGH);
       // WDTCR = (1<<WDIE) | (0<<WDP0);
-      WDTCR = (1<<WDIE) | (1<<WDP2) | (1<<WDP1);
+      WDTCR = (1<<WDIE) | (1<<WDP0);
       onTime++;
       color++;
-      strip.clear();
-      strip.setPixelColor(currentPix, Wheel(color));
-      strip.show();
       if(currentPix == 4) {
         currentPix = 0;
       }
       else {
         currentPix++;
       }
+      leds[0] = CRGB::Black;
+      leds[1] = CRGB::Black;
+      leds[2] = CRGB::Black;
+      leds[3] = CRGB::Black;
+      leds[4] = CRGB::Black;
+
+      hue += hueDelta;
+      // leds[currentPix] = CRGB::White;
+      leds[currentPix].setHue(hue);
+      FastLED.show();
     }
     else {
-      // turn off
-      // WDTCR = (1<<WDIE) | (1<<WDP2) | (1<<WDP1);
-      digitalWrite(PORTB1, LOW);
-      strip.clear();
-      strip.show();
     }
   }
 }
@@ -92,21 +121,6 @@ void nightyNight() {
   sleep_mode();
 }
 
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else if(WheelPos < 170) {
-    WheelPos -= 85;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  }
-}
 
 
 ISR(WDT_vect) {
